23a7fa67203bb39e38c6c614af9b5bdd0880dc04
Codey Pinero <codey.pinero@gmail.com>
2019-04-08 04:00:48 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser
-  def initialize(email)
+  def initialize(email, email_two)
     @email = email
   end
 end
